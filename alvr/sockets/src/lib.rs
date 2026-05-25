7a929b0599e80fd68a863871ff91f8e56616de21
UNKNOWNツ <UNKNOWNV5@hotmail.com>
2026-05-25 21:02:29 +0200

    Add files via upload

---
+++ alvr/sockets/src/lib.rs
@@ -0,0 +1,90 @@
+mod control_socket;
+mod stream_socket;
+
+use alvr_common::{anyhow::Result, info};
+use alvr_session::{DscpTos, SocketBufferConfig, SocketBufferSize};
+use socket2::Socket;
+use std::{
+    net::{IpAddr, Ipv4Addr},
+    time::Duration,
+};
+
+pub use control_socket::*;
+pub use stream_socket::*;
+
+pub const LOCAL_IP: IpAddr = IpAddr::V4(Ipv4Addr::UNSPECIFIED);
+pub const CONTROL_PORT: u16 = 9943;
+pub const HANDSHAKE_PACKET_SIZE_BYTES: usize = 56; // this may change in future protocols
+pub const KEEPALIVE_INTERVAL: Duration = Duration::from_millis(500);
+pub const KEEPALIVE_TIMEOUT: Duration = Duration::from_secs(2);
+
+pub const MDNS_SERVICE_TYPE: &str = "_alvr._tcp.local.";
+pub const MDNS_PROTOCOL_KEY: &str = "protocol";
+pub const MDNS_DEVICE_ID_KEY: &str = "device_id";
+
+pub const WIRED_CLIENT_HOSTNAME: &str = "client.wired";
+
+fn set_socket_buffers(socket: &socket2::Socket, buffer_config: SocketBufferConfig) -> Result<()> {
+    info!(
+        "Initial socket buffer size: send: {}B, recv: {}B",
+        socket.send_buffer_size()?,
+        socket.recv_buffer_size()?
+    );
+
+    {
+        let maybe_size = match buffer_config.send_size_bytes {
+            SocketBufferSize::Default => None,
+            SocketBufferSize::Maximum => Some(u32::MAX),
+            SocketBufferSize::Custom(size) => Some(size),
+        };
+
+        if let Some(size) = maybe_size {
+            if let Err(e) = socket.set_send_buffer_size(size as usize) {
+                info!("Error setting socket send buffer: {e}");
+            } else {
+                info!(
+                    "Set socket send buffer succeeded: {}",
+                    socket.send_buffer_size()?
+                );
+            }
+        }
+    }
+
+    {
+        let maybe_size = match buffer_config.recv_size_bytes {
+            SocketBufferSize::Default => None,
+            SocketBufferSize::Maximum => Some(u32::MAX),
+            SocketBufferSize::Custom(size) => Some(size),
+        };
+
+        if let Some(size) = maybe_size {
+            if let Err(e) = socket.set_recv_buffer_size(size as usize) {
+                info!("Error setting socket recv buffer: {e}");
+            } else {
+                info!(
+                    "Set socket recv buffer succeeded: {}",
+                    socket.recv_buffer_size()?
+                );
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn set_dscp(socket: &Socket, dscp: Option<DscpTos>) {
+    // https://en.wikipedia.org/wiki/Differentiated_services
+    if let Some(dscp) = dscp {
+        let tos = match dscp {
+            DscpTos::BestEffort => 0,
+            DscpTos::ClassSelector(precedence) => precedence << 3,
+            DscpTos::AssuredForwarding {
+                class,
+                drop_probability,
+            } => (class << 3) | drop_probability as u8,
+            DscpTos::ExpeditedForwarding => 0b101110,
+        };
+
+        socket.set_tos_v4((tos << 2) as u32).ok();
+    }
+}
